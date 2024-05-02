7134f72f595fb36aabaff81dbf08107d9786017c
MRTzee <teukumuksalmina280800@gmail.com>
2024-05-02 23:47:00 +0700

    feat: login and forgot password

--- apps/web/src/redux/store.ts
+++ apps/web/src/redux/store.ts
@@ -1,15 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from "@/redux/slices/userSlice"
-
+import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '@/redux/slices/userSlice';
 
 export const makeStore = () => {
   return configureStore({
     reducer: {
-        user: userReducer,
-    }
-  })
-}
+      user: userReducer,
+    },
+  });
+};
 
-export type AppStore = ReturnType<typeof makeStore>
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
+export type AppStore = ReturnType<typeof makeStore>;
+export type RootState = ReturnType<AppStore['getState']>;
+export type AppDispatch = AppStore['dispatch'];
