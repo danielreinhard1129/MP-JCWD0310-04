2f183cd8b057bc2a65a0bba51b600d22de6d77c8
Teuku Muksal Mina <93637614+MRTzee@users.noreply.github.com>
2024-05-04 15:26:39 +0700

    feat: login and forgot password (#5)

--- apps/api/src/controllers/auth.controller.ts
+++ apps/api/src/controllers/auth.controller.ts
@@ -1,3 +1,5 @@
+import { forgotPasswordService } from '@/services/auth/forgot-password.service';
+import { loginService } from '@/services/auth/login.service';
 import { registerService } from '@/services/auth/register.service';
 import { NextFunction, Request, Response } from 'express';
 
@@ -5,6 +7,28 @@ export class AuthController {
   async registerController(req: Request, res: Response, next: NextFunction) {
     try {
       const result = await registerService(req.body);
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async loginController(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await loginService(req.body);
+      res.status(200).send(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async forgotPasswordController(
+    req: Request,
+    res: Response,
+    next: NextFunction,
+  ) {
+    try {
+      const result = await forgotPasswordService(req.body.email);
       res.status(200).send(result);
     } catch (error) {
       next(error);
--- apps/web/src/redux/slices/userSlice.ts
+++ apps/web/src/redux/slices/userSlice.ts
@@ -1,29 +1,26 @@
-import { User } from "@/types/user.type";
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-
-
+import { User } from '@/types/user.type';
+import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 const initialState: User = {
-    id: 0,
-    userName: "",
-    email: "",
-}
+  id: 0,
+  userName: '',
+  email: '',
+};
 
 export const userSlice = createSlice({
-    name: "user",
-    initialState,
-    reducers: {
-        loginAction: (state, action: PayloadAction<User>) => {
-            state.id = action.payload.id,
-                state.userName = action.payload.userName,
-                state.email = action.payload.email
-        },
-        logoutAction: (state) => {
-            state.id = 0
-            state.userName = "",
-                state.email = ""
-        },
+  name: 'user',
+  initialState,
+  reducers: {
+    loginAction: (state, action: PayloadAction<User>) => {
+      (state.id = action.payload.id),
+        (state.userName = action.payload.userName),
+        (state.email = action.payload.email);
     },
+    logoutAction: (state) => {
+      state.id = 0;
+      (state.userName = ''), (state.email = '');
+    },
+  },
 });
 
 export const { loginAction, logoutAction } = userSlice.actions;
