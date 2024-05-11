685883565eb32dcdf4f7ec5773c26a56db3ee91d
Teuku Muksal Mina <93637614+MRTzee@users.noreply.github.com>
2024-05-11 20:55:08 +0700

    fix: navbar and image (#26)

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -5,11 +5,13 @@ import express, {
   Request,
   Response,
   NextFunction,
+  static as static_,
 } from 'express';
 import cors from 'cors';
 import { PORT } from './config';
 import { AuthRouter } from './routers/auth.router';
 import { EventRouter } from './routers/event.router';
+import { join } from 'path';
 
 export default class App {
   private app: Express;
@@ -25,6 +27,7 @@ export default class App {
     this.app.use(cors());
     this.app.use(json());
     this.app.use(urlencoded({ extended: true }));
+    this.app.use('/api/assets', static_(join(__dirname, '../public')));
   }
 
   private handleError(): void {
