567381ee0f4150950efdd3390b6fb71f48728f8c
Dominique Liza E <139145333+dominiquelev@users.noreply.github.com>
2024-05-16 00:32:51 +0700

    fix: layout dashboard admin (#33)

--- apps/web/src/app/(admin)/statistic/page.tsx
+++ apps/web/src/app/(admin)/statistic/page.tsx
@@ -1,9 +1,18 @@
-'use client';
-
-import React from 'react';
+import Chart from "../dashboard/components/Chart";
 
 const Statistic = () => {
-  return <div>Statistic</div>;
+  return (
+    <section className="container h-screen w-full ">
+      <div className="text-4xl font-bold p-5 mt-10">
+        <h1>Statistic </h1>
+      </div>
+      <div>
+        <Chart/>
+      </div>
+
+      
+    </section>
+  );
 };
 
 export default Statistic;
--- apps/web/tailwind.config.js
+++ apps/web/tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
   content: [
     "./src/**/*.{js,ts,jsx,tsx,mdx}"
   ],
@@ -72,5 +74,5 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+})
 
